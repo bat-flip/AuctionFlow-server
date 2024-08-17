6d1dbeefa8d980e0e835bf948741b793093187e3
hyunji <phj9573@naver.com>
2024-08-17 16:28:46 +0900

    first commit

---
+++ src/main/java/org/example/auctionflowserver/AuctionFlowServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.auctionflowserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionFlowServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionFlowServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/auctionflowserver/AuctionFlowServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.auctionflowserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionFlowServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
