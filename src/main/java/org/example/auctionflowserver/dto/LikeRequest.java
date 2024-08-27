fad73ccdd1b3bd7146b1884e110dcd91758148f9
hyunji <phj9573@naver.com>
2024-08-27 19:47:26 +0900

    Feat : 상품 찜 추가, 삭제

---
+++ src/main/java/org/example/auctionflowserver/dto/LikeRequest.java
@@ -0,0 +1,10 @@
+package org.example.auctionflowserver.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LikeRequest {
+    private Long itemId;
+}
--- src/main/java/org/example/auctionflowserver/repository/LikeRepository.java
+++ src/main/java/org/example/auctionflowserver/repository/LikeRepository.java
@@ -1,13 +1,17 @@
 package org.example.auctionflowserver.repository;
 
+import org.example.auctionflowserver.entity.Item;
 import org.example.auctionflowserver.entity.Like;
 import org.example.auctionflowserver.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
     List<Like> findByUser(User user);
+
+    Optional<Like> findByUserAndItem(User user, Item item);
 }
