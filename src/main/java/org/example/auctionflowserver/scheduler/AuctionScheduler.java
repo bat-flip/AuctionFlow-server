2cdfab6701afbdbc6c80c84bf142163b1de86e22
yoon hyeon soo <chris24da@naver.com>
2024-08-31 10:34:29 +0900

    feature: 경매 종료시 최고 입찰자 userID 소켓 전송

--- src/main/java/org/example/auctionflowserver/scheduler/AuctionScheduler.java
+++ src/main/java/org/example/auctionflowserver/scheduler/AuctionScheduler.java
@@ -1,5 +1,6 @@
 package org.example.auctionflowserver.scheduler;
 
+import org.example.auctionflowserver.entity.Bid;
 import org.example.auctionflowserver.entity.Item;
 import org.example.auctionflowserver.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,9 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class AuctionScheduler {
@@ -31,11 +34,29 @@ public class AuctionScheduler {
                 })
                 .toList();
 
+
         for (Item item : activeItems) {
+            Optional<Bid> highestBid = item.getBids().stream()
+                    .max(Comparator.comparing(Bid::getBidAmount));
+
             item.setItemBidStatus("end");
             itemService.saveItem(item);
 
-            messagingTemplate.convertAndSend("/topic/auction/" + item.getItemId(), "경매가 종료되었습니다.");
+            if (highestBid.isPresent()) {
+                String message = String.format("경매가 종료되었습니다. 최고 입찰자는 사용자 ID: %d, 입찰 금액: %s원입니다.",
+                        highestBid.get().getUser().getUserId(),
+                        highestBid.get().getBidAmount().toString());
+                messagingTemplate.convertAndSend("/topic/auction/" + item.getItemId(), message);
+            } else {
+                messagingTemplate.convertAndSend("/topic/auction/" + item.getItemId(), "경매가 종료되었습니다. 입찰자가 없습니다.");
+            }
         }
+
+//        for (Item item : activeItems) {
+//            item.setItemBidStatus("end");
+//            itemService.saveItem(item);
+//
+//            messagingTemplate.convertAndSend("/topic/auction/" + item.getItemId(), "경매가 종료되었습니다.");
+//        }
     }
 }
